feebcaa3c55ab57200522f849f010ba4aeefe3db
Olga <oaglazunova@gmail.com>
2015-12-20 18:58:52 +0300

    02. Embedding example finished

--- js/embed.js
+++ js/embed.js
@@ -1,17 +1,3 @@
-'use strict';
-
-/* Menu */
-var toggler = document.getElementById('toggler');
-var header = document.getElementById('header');
-var menu = document.getElementById('menu');
-
-toggler.onclick = function (e) {
-  e.preventDefault();
-  toggler.classList.toggle('menu-toggler--close');
-  menu.classList.toggle('main-nav--vertical');
-};
-
-/* JSON */
 $(document).ready(function () {
   loadProjects();
 });
